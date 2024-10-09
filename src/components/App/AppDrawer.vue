70024d413ab75464d6d42fc3569cd8122ea29d9d
HazemHamdyA <hazemhamdya>
2024-10-09 07:25:11 +0300

    clean

--- src/components/App/AppDrawer.vue
+++ src/components/App/AppDrawer.vue
@@ -82,25 +82,6 @@ const modules = ref([
             <span class="ms-3" v-if="!isDrawerOpen">{{ module.name }}</span>
           </router-link>
         </li>
-        <!-- <li>
-          <router-link
-            to="#"
-            class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-          >
-            <AppSvgIcon
-              name="kanban"
-              class="w-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-            />
-
-            <template v-if="!isDrawerOpen">
-              <span class="flex-1 ms-3 whitespace-nowrap">Kanban</span>
-              <span
-                class="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300"
-                >Pro</span
-              >
-            </template>
-          </router-link>
-        </li> -->
       </ul>
     </div>
   </aside>
